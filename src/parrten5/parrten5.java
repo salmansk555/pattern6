6a2a738d5bc40ae280202e33daeb8ae306174c4c
salmansk555 <salmanskk158@gmail.com>
2024-12-24 11:26:18 +0530

    first commit

---
+++ src/parrten5/parrten5.java
@@ -0,0 +1,42 @@
+package parrten5;
+
+public class parrten5 {
+
+
+
+ 
+	
+	public static void main(String[] abc) {
+		
+		/*
+		 * 1
+		 * 0 1
+		 * 1 0 1
+		 * 0 1 0 1
+		 * 
+		 */
+		
+		
+		for(int r =1 ;r<=4;r++) {
+			if(r % 2 == 0) {
+				// even
+				for(int c=1,p=0;c<=r;c++) {
+					System.out.print(p+" ");// 3 % 2----1
+					if(p==1) p=0;
+					else p=1;
+				}
+			}
+			else {
+				// odd
+				for(int c=1,p=1;c<=r;c++) {
+					System.out.print(p+" ");// 3 % 2----1
+					if(p==1) p=0;
+					else p=1;
+				}
+			}// else
+			System.out.println();
+		}
+		
+	}
+
+}
